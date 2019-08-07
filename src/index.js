4f97cdd9a36448cdf08d5997b91e0c4e591d4d6c
Vinnie <vz377@nyu.edu>
2019-08-07 00:13:32 -0500

    changed comments on index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import 'font-awesome/css/font-awesome.min.css';
 import { HashRouter as Router } from 'react-router-dom';
 
 // for use on actual website
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App />, document.getElementById('root')</Router>);
+//ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App />, document.getElementById('root')</Router>);
 
 // for use on local machine
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
